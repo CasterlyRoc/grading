1f28ceac57b2cfa79e5220b4c158663920178b45
Casterly_Roc <zknopp@terpmail.umd.edu>
2015-10-01 16:36:10 -0400

    t

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,53 @@
 class StudentsController < ApplicationController
+	
+	before_action: get_student, only:[:show,:edit,:update,:destroy] 
+
+	def get_student
+		@student = Student.find(params[:id])
+	end
+
 	def index
     	@students = Student.all
   	end
+
+  	def show
+  		@student = Student.find(params[:id])
+  	end
+
+  	def new
+  		@student = Student.new
+  	end
+
+  	def edit
+
+  	end
+
+  	def create
+  		@student = Student.new(student_params)
+  		if @student.save
+  			redirect_to @student
+  		else
+  			render 'new'
+  		end
+  	end
+
+  	def update
+  		if @student.update(student_params)
+  			redirect_to @student
+  		else
+  			render 'edit'
+
+  		end
+  	end
+
+  	def destroy
+	    @student.destroy
+	 
+	    redirect_to students_path
+  	end
+
+  	private
+    def student_params
+      params.require(:student).permit(:name, :address)
+    end
 end
