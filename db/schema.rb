f4bd17ab86b47f3a2c759dfd798cb2cbbc09e837
Casterly_Roc <zknopp@terpmail.umd.edu>
2015-10-04 14:26:58 -0400

    t

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151004175805) do
+ActiveRecord::Schema.define(version: 20151004182524) do
 
   create_table "assignments", force: :cascade do |t|
     t.string   "due_date"
@@ -37,6 +37,17 @@ ActiveRecord::Schema.define(version: 20151004175805) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "question"
+    t.string   "answer"
+    t.integer  "point_value"
+    t.integer  "assignemt_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "questions", ["assignemt_id"], name: "index_questions_on_assignemt_id"
 
   create_table "students", force: :cascade do |t|
     t.string   "name"
