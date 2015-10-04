ffde27c785a1d4f99d1561e36ed07c7efaafb785
CasterlyRoc <zknopp@terpmail.umd.edu>
2015-10-04 14:14:35 -0400

    Assignment

---
+++ app/controllers/assignments_controller.rb
@@ -0,0 +1,21 @@
+class AssignmentsController  ApplicationController
+  def new
+    @assignment = Assignment.new
+  end
+
+  def create
+    @assignment = Assignment.new(assignment_params)
+
+    if @assignment.save
+      redirect_to @assignment
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def assignment_params
+      params.require(:assignment).permit(:due_date, :tot_points)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
-  resources :instructors 
+  resources :instructors
   resources :courses
   resources :students
+  resources :assignments
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
