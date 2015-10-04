17c79f0f22f8d2c6bd1192e87953b3e4ae61095b
Casterly_Roc <zknopp@terpmail.umd.edu>
2015-10-04 13:39:44 -0400

    t

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921231619) do
+ActiveRecord::Schema.define(version: 20151004173815) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "instructor_id"
   end
+
+  add_index "courses", ["instructor_id"], name: "index_courses_on_instructor_id"
 
   create_table "instructors", force: :cascade do |t|
     t.string   "name"
